f7a3966e11f9eb5f56c1cd2bbeec4f271a7d22a4
lucas <lucas.pfranca01@gmail.com>
2024-06-27 16:05:22 -0300

    fix: dotenv for env variables

--- src/utils/config/env.ts
+++ src/utils/config/env.ts
@@ -1,4 +1,10 @@
-import { z } from 'zod'
+import { z } from 'zod';
+import dotenv from 'dotenv';
+import fs from 'fs';
+
+
+const envFile = dotenv.parse(fs.readFileSync('.env'));
+
 
 const envSchema = z.object({
   NEXT_PUBLIC_BASE_URL: z.string().url(),
@@ -6,16 +12,18 @@ const envSchema = z.object({
   NEXT_PUBLIC_API_KEY: z.string(),
   NEXT_PUBLIC_STRIPE_PUBLIC_KEY: z.string(),
   STRIPE_SECRET_KEY: z.string()
-})
+});
 
-const parsedEnv = envSchema.safeParse(process.env)
+
+const parsedEnv = envSchema.safeParse(envFile);
 
 if (!parsedEnv.success) {
   console.error(
     'Invalid environment variables',
     parsedEnv.error.flatten().fieldErrors,
-  )
-  throw new Error('Invalid environment variables.')
+  );
+  throw new Error('Invalid environment variables.');
 }
 
-export const env = parsedEnv.data
+// Exportar as variáveis de ambiente validadas
+export const env = parsedEnv.data;
