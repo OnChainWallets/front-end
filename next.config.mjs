c0d667b61912b5efd37f62f50649abd05c5abb84
lucas <lucas.pfranca01@gmail.com>
2024-06-28 17:58:03 -0300

    fix: remove line from next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL
-      },
     images: {
         remotePatterns: [
             {
